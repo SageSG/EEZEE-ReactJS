06a396a47d2b97888d2cd65f6b72d75981d7d9de
SageSG <houliangyip@gmail.com>
2022-09-01 02:24:05 +0800

    Code commenting and Documentation

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlinePhone } from "react-icons/ai";
 import SGflag from "../../assets/vectors/singaporeflag.png";
 import Eezeelogo from "../../assets/vectors/eezeelogo.png";
@@ -7,15 +7,17 @@ import { Link, useNavigate } from "react-router-dom";
 
 function Nav() {
   let navigate = useNavigate();
-  const [sessionItems, setSessionItems] = useState(0)
-  useEffect(()=>{
+  const [sessionItems, setSessionItems] = useState(0);
+
+  // Use session storage to save cart items temporarily, acts like a Vuex store
+  useEffect(() => {
     let cartItems = sessionStorage.getItem("cartItems");
-    if(cartItems){
-      setSessionItems(cartItems)
-    }else{
-      setSessionItems(0)
+    if (cartItems) {
+      setSessionItems(cartItems);
+    } else {
+      setSessionItems(0);
     }
-},[])
+  }, []);
   return (
     <section id="Nav">
       <div className="flex w-full h-[38px] bg-[#EFEFF0] items-center">
@@ -42,14 +44,25 @@ function Nav() {
       </div>
       <div className="flex flex-wrap w-full h-[119px] border-b-[1px] border-[#D7D8DB] bg-[#FFFFFF]">
         <div className="flex flex-row w-full h-[80px] justify-between items-center mx-[120px]">
-          <img src={Eezeelogo} className="w-[150px] h-[44px]" alt={''} onClick={()=>{navigate('/')}} />
+          <img
+            src={Eezeelogo}
+            className="w-[150px] h-[44px]"
+            alt={""}
+            onClick={() => {
+              navigate("/");
+            }}
+          />
           <div className="flex flex-col w-[40px] h-[60px] self-start items-center justify-center">
             <div className="relative h-[40px] w-[40px] mt-[14px]">
               <span className="absolute top-0 right-[-8px] w-[20px] h-[20px] rounded-[22px] bg-[#2A64DB] font-normal text-[12px] leading-[20px] text-[#FFFFFF] text-center top-[6px] font-roboto">
                 {sessionItems}
               </span>
             </div>
-            <img src={Carticon} className="w-[40px] h-[40px] mt-[14px]" alt={''}/>
+            <img
+              src={Carticon}
+              className="w-[40px] h-[40px] mt-[14px]"
+              alt={""}
+            />
 
             <div className="w-[24px] h-[20px] text-[12px] leading-[20px] text-center text-[#55585D] font-bold font-roboto">
               Cart
@@ -57,7 +70,12 @@ function Nav() {
           </div>
         </div>
         <div className="flex w-full h-[38px] mx-[120px] items-center">
-          <Link to="/brands" className="cursor-pointer w-[97px] h-[22px] font-normal text-[14px] leading-[22px] text-[#2A64DB] font-roboto">View All Brands</Link>
+          <Link
+            to="/brands"
+            className="cursor-pointer w-[97px] h-[22px] font-normal text-[14px] leading-[22px] text-[#2A64DB] font-roboto"
+          >
+            View All Brands
+          </Link>
         </div>
       </div>
     </section>
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useLayoutEffect } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { MdArrowForwardIos } from "react-icons/md";
+
+// Add the banner images
 import Logo1 from "../assets/vectors/coupa-logo.png";
 import Logo2 from "../assets/vectors/ivalua-logo.png";
 import Logo3 from "../assets/vectors/gep-smart-logo.png";
@@ -10,16 +12,17 @@ import Logo6 from "../assets/vectors/sap-logo.png";
 import Logo7 from "../assets/vectors/sap-ariba-logo.png";
 import Logo8 from "../assets/vectors/tenderboard-logo.png";
 
+// Mock datasets to render
 import dataBrands from "../assets/datasets/brands.json";
 import dataProducts from "../assets/datasets/products.json";
 
 function Homepage() {
   const [featureBrands, setFeatureBrands] = useState([]);
   const [productsWidth, setProductsWidth] = useState(0);
-  let navigate = useNavigate()
+  let navigate = useNavigate();
+
   useEffect(() => {
     let brands = [];
-
     dataBrands.forEach(function (brand) {
       // Show the featured brand in the list
       if (brand.featured) {
@@ -29,26 +32,27 @@ function Homepage() {
     }, false);
   }, []);
 
+  // To change the section width at the start of page render
   useEffect(() => {
-    resizeProductsBox()
-}, []);
-
-function resizeProductsBox(){
-    setProductsWidth((document.getElementById("Products").offsetWidth-41)/6)
-}
-
-
-// Resize the width if the window is changed
-useLayoutEffect(() => {
-    function updateSize() {
-      resizeProductsBox()
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
+    resizeProductsBox();
   }, []);
 
+  // Change the products to scale with gap, did not use grid as it is unstable
+  function resizeProductsBox() {
+    setProductsWidth(
+      (document.getElementById("Products").offsetWidth - 41) / 6
+    );
+  }
 
+  // Resize the width if the window is changed
+  useLayoutEffect(() => {
+    function updateSize() {
+      resizeProductsBox();
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
   return (
     <main className="flex flex-col flex-wrap w-full h-full px-[120px] pb-[119px] bg-[#EFEFF0] items-center">
@@ -96,7 +100,10 @@ useLayoutEffect(() => {
           <p className="text-[#55585D] text-[14px] leading-[16px] font-roboto font-normal">
             Browse the full catalog of brands today
           </p>
-          <span className="flex font-roboto font-normal text-[#2A64DB] text-[14px] leading-[22px]" onClick={()=>navigate('brands')}>
+          <span
+            className="flex font-roboto font-normal text-[#2A64DB] text-[14px] leading-[22px]"
+            onClick={() => navigate("brands")}
+          >
             View More{" "}
             <MdArrowForwardIos className="self-center w-[14px] h-[14px] ml-[4px] text-[#2A64DB] leading-[28px]" />
           </span>
@@ -107,12 +114,12 @@ useLayoutEffect(() => {
               <div
                 key={index}
                 className="cursor-pointer flex flex-col flex-wrap w-1/6 h-[157px] bg-white border-[1px] border-[#E4E5E7] rounded"
-                onClick={()=>navigate('brands')}
+                onClick={() => navigate("brands")}
               >
                 <img
                   src={brand.image.url}
                   className="flex h-3/5 w-full object-contain"
-                  alt={''}
+                  alt={""}
                 />
                 <div className="flex h-1/5 w-full justify-center pt-[8px] text-[16px] leading-[20px] font-bold font-roboto text-[#242528]">
                   {brand.name}
@@ -134,45 +141,94 @@ useLayoutEffect(() => {
           <p className="text-[#55585D] text-[14px] leading-[16px] font-roboto font-normal">
             Trusted by the best companies in Asia
           </p>
-          <span className="flex font-roboto font-normal text-[#2A64DB] text-[14px] leading-[22px]" onClick={()=>navigate('products')}>
+          <span
+            className="flex font-roboto font-normal text-[#2A64DB] text-[14px] leading-[22px]"
+            onClick={() => navigate("products")}
+          >
             View More{" "}
             <MdArrowForwardIos className="self-center w-[14px] h-[14px] ml-[4px] text-[#2A64DB] leading-[28px]" />
           </span>
         </div>
-        <div id="Products" className="flex flex-row flex-wrap w-full mt-[12px] gap-[8px]">
-            {dataProducts.sort(() => Math.random() - 0.5).map((product, index) => {
-            return (
-                <div key={index} style={{width:productsWidth + 'px' }} className="cursor-pointer flex flex-col h-[333px] justify-center align-center bg-white border-[1px] border-[#E4E5E7] rounded px-[9px]" onClick={()=>navigate('products')}>
-                <div className="flex flex-col h-[184px] p-[8px] w-full">
-                <img
-                className={'' + (product.vipPriceFlag && product.bulkDiscountFlag) ? 'w-[104px] self-center mb-[6px]': (product.vipPriceFlag || product.bulkDiscountFlag) ? 'w-[120px] self-center mb-[10px]': ''}
-                  src={product.images[0].url}
-                  alt={JSON.stringify(product.metadata)}
-                />
-                {product.vipPriceFlag && <div className="flex h-fit w-fit bg-[#DBE5FA] px-[4px] rounded-sm"><span className="font-roboto font-bold text-[12px] leading-[20px] text-[#1E4DAF]">VIP Price</span></div>}
-                {product.bulkDiscountFlag && <div className="flex h-fit w-fit bg-[#FFE69C] px-[4px] rounded-sm"><span className="font-roboto font-bold text-[12px] leading-[20px] text-[#6A5001]">Bulk Discount</span></div>}
-                {(product.vipPriceFlag || product.bulkDiscountFlag) && <div className="flex h-fit w-fit bg-[#EFEFF0] px-[4px] rounded-sm"><span className="font-roboto font-bold text-[12px] leading-[20px] text-[#242528]">MOQ: {product.moq}</span></div>}
-                </div>
-                
-                
+        <div
+          id="Products"
+          className="flex flex-row flex-wrap w-full mt-[12px] gap-[8px]"
+        >
+          {dataProducts
+            .sort(() => Math.random() - 0.5)
+            .map((product, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{ width: productsWidth + "px" }}
+                  className="cursor-pointer flex flex-col h-[333px] justify-center align-center bg-white border-[1px] border-[#E4E5E7] rounded px-[9px]"
+                  onClick={() => navigate("products")}
+                >
+                  <div className="flex flex-col h-[184px] p-[8px] w-full">
+                    <img
+                      className={
+                        "" + (product.vipPriceFlag && product.bulkDiscountFlag)
+                          ? "w-[104px] self-center mb-[6px]"
+                          : product.vipPriceFlag || product.bulkDiscountFlag
+                          ? "w-[120px] self-center mb-[10px]"
+                          : ""
+                      }
+                      src={product.images[0].url}
+                      alt={JSON.stringify(product.metadata)}
+                    />
+                    {product.vipPriceFlag && (
+                      <div className="flex h-fit w-fit bg-[#DBE5FA] px-[4px] rounded-sm">
+                        <span className="font-roboto font-bold text-[12px] leading-[20px] text-[#1E4DAF]">
+                          VIP Price
+                        </span>
+                      </div>
+                    )}
+                    {product.bulkDiscountFlag && (
+                      <div className="flex h-fit w-fit bg-[#FFE69C] px-[4px] rounded-sm">
+                        <span className="font-roboto font-bold text-[12px] leading-[20px] text-[#6A5001]">
+                          Bulk Discount
+                        </span>
+                      </div>
+                    )}
+                    {(product.vipPriceFlag || product.bulkDiscountFlag) && (
+                      <div className="flex h-fit w-fit bg-[#EFEFF0] px-[4px] rounded-sm">
+                        <span className="font-roboto font-bold text-[12px] leading-[20px] text-[#242528]">
+                          MOQ: {product.moq}
+                        </span>
+                      </div>
+                    )}
+                  </div>
 
-                
-                <div className="flex flex-col flex-nowrap w-full h-[140px] mt-[8px]">
-                  {product.highPriceOriginal && <div className="text-[12px] leading-[20px] text-[#868A92] font-roboto font-normal line-through">
-                    {product.highPriceOriginalPretty}
-                  </div>}
-                  <div className="flex items-stretch text-[16px] leading-[20px] text-[#2A64DB] font-roboto font-bold">
-                  <span className="text-[10px] leading-[12px] top-0 self-start">{product.currencySymbol}</span> <span>{product.lowPrice && product.lowPrice.toFixed(2)}</span> {product.highPrice ? <span className="mx-[4px]">–</span> : null} <span className="text-[10px] leading-[12px] top-0 self-start">{product.highPrice && product.currencySymbol}</span><span>{product.highPrice && product.highPrice.toFixed(2)}</span>
-                  </div>
-                  <div className="mt-[10px] font-roboto font-normal text-[#242528] text-[14px] leading-[16px]">
-                    {product.title}
+                  <div className="flex flex-col flex-nowrap w-full h-[140px] mt-[8px]">
+                    {product.highPriceOriginal && (
+                      <div className="text-[12px] leading-[20px] text-[#868A92] font-roboto font-normal line-through">
+                        {product.highPriceOriginalPretty}
+                      </div>
+                    )}
+                    <div className="flex items-stretch text-[16px] leading-[20px] text-[#2A64DB] font-roboto font-bold">
+                      <span className="text-[10px] leading-[12px] top-0 self-start">
+                        {product.currencySymbol}
+                      </span>{" "}
+                      <span>
+                        {product.lowPrice && product.lowPrice.toFixed(2)}
+                      </span>{" "}
+                      {product.highPrice ? (
+                        <span className="mx-[4px]">–</span>
+                      ) : null}{" "}
+                      <span className="text-[10px] leading-[12px] top-0 self-start">
+                        {product.highPrice && product.currencySymbol}
+                      </span>
+                      <span>
+                        {product.highPrice && product.highPrice.toFixed(2)}
+                      </span>
+                    </div>
+                    <div className="mt-[10px] font-roboto font-normal text-[#242528] text-[14px] leading-[16px]">
+                      {product.title}
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
-        
       </section>
     </main>
   );
